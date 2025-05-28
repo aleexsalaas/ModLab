a8b7d42fc19f101bbf44e6c7859d966ffb136172
Alex Salas Galán <galsaan@MacBook-Air-de-Alex.local>
2025-05-28 11:08:06 +0200

    rutas y dist

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,10 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://modlab.com/api/users'  // URL para producción
+    apiUrl: 'http://modlabfront.onrender.com/modlab/User',
+    apiUrlShippingAddress: 'http://modlabfront.onrender.com/modlab/ShippingAddress',
+    apiUrlCpu: 'http://modlabfront.onrender.com/modlab/CPU',
+    apiUrlReview: 'http://modlabfront.onrender.com/modlab/Review',
+    apiUrlProduct: 'http://modlabfront.onrender.com/modlab/products',
+    apiUrlPaymentMethod: 'http://modlabfront.onrender.com/modlab/paymentMethod', 
   };
   
