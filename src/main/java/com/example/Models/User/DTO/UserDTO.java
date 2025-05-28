18e5651c26aa62cb961521f4d73abaf203742172
Alex Salas Galán <galsaan@MacBook-Air-de-Alex.local>
2025-05-28 17:59:36 +0200

    user

--- src/main/java/com/example/Models/User/DTO/UserDTO.java
+++ src/main/java/com/example/Models/User/DTO/UserDTO.java
@@ -16,7 +16,7 @@ import jakarta.persistence.Table;
 public class UserDTO {
 
     @Id
-    @Column(name = "userId")
+    @Column(name = "user_id")
     private String userId;
 
     @Column(name = "first_name", nullable = false)
